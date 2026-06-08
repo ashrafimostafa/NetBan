374d035ad7c7b6282068f38f3e31374aa52a9776
Mostafa <ashrafimostafa@gmail.com>
2026-06-08 16:56:48 +0330

    Version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mostafa.netban"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         base.archivesName = "$namespace-$versionName"
 
     }
